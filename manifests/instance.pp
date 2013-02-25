0b4bb27e7c1d1d9d22b244255bf12b1bdba1f6c9
Tom De Vylder <tom@penumbra.be>
2013-02-25 14:43:00 +0100

    Switch to soure balancing algoritm by default

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -2,7 +2,7 @@
 #
 define haproxy::instance (
   $ensure          = present,
-  $balance         = 'leastconn',
+  $balance         = 'source',
   $bind            = '*:80',
   $default_backend = undef,
   $instance        = 'listen',
