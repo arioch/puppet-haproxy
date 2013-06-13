9d157a64d1d204368eddbdb4fdd2c2c070846d2b
Tom De Vylder <tom@penumbra.be>
2013-06-13 10:50:13 +0200

    Add missing parameter: service_hasrestart

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,27 +3,30 @@
 class haproxy::service {
   if $::haproxy::service_dep {
     service { $::haproxy::service_dep:
-      ensure    => $::haproxy::service_ensure,
-      enable    => $::haproxy::service_enable,
-      hasstatus => $::haproxy::service_hasstatus,
-      require   => Class['::haproxy::config'];
+      ensure     => $::haproxy::service_ensure,
+      enable     => $::haproxy::service_enable,
+      hasrestart => $::haproxy::service_hasrestart,
+      hasstatus  => $::haproxy::service_hasstatus,
+      require    => Class['::haproxy::config'];
     }
 
     service { $::haproxy::service_name:
-      ensure    => $::haproxy::service_ensure,
-      enable    => $::haproxy::service_enable,
-      hasstatus => $::haproxy::service_hasstatus,
-      require   => [
+      ensure     => $::haproxy::service_ensure,
+      enable     => $::haproxy::service_enable,
+      hasrestart => $::haproxy::service_hasrestart,
+      hasstatus  => $::haproxy::service_hasstatus,
+      require    => [
         Service['::haproxy::service_dep'],
         Class['::haproxy::config'],
       ];
     }
   } else {
     service { $::haproxy::service_name:
-      ensure    => $::haproxy::service_ensure,
-      enable    => $::haproxy::service_enable,
-      hasstatus => $::haproxy::service_hasstatus,
-      require   => Class['::haproxy::config'];
+      ensure     => $::haproxy::service_ensure,
+      enable     => $::haproxy::service_enable,
+      hasrestart => $::haproxy::service_hasrestart,
+      hasstatus  => $::haproxy::service_hasstatus,
+      require    => Class['::haproxy::config'];
     }
   }
 }
